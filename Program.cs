73816f4ea3c93700135c56e11d5339d26999dd36
Alan Sebastian <alansebastian4@gmail.com>
2021-07-25 14:49:08 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.IO;
+
+namespace phpMyAdmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ReadAndUploadImage();
+        }
+
+
+        static void ReadAndUploadImage()
+        {
+            FileStream fileStream = new FileStream("image.jpg", FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                using var image = Image.Load(fileStream);
+                image.Mutate(x => x.Resize(150, 150));
+                image.Save("image-150X150.jpg");
+            }
+           
+        }
+    }
+}
